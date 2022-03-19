ade68c9c83183117386bdf2d23bc91b99ac07d0a
Srijan Kr. Gupta <gupta.srijan94@gmail.com>
2022-03-19 16:57:29 +0530

    `feat:` routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Banner from './components/Banner'
+import Footer from './components/Footer'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Teams from './components/Teams'
+import AppBar from './components/AppBar'
+import { ThemeProvider } from '@mui/material/styles'
+import theme from './theme'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+      <AppBar />
+      <Router>
+        <Routes>
+          <Route path="/teams/" element={<Teams />}></Route>
+          <Route path="/" element={<Banner />}></Route>
+        </Routes>
+      </Router>
+      <Footer />
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
